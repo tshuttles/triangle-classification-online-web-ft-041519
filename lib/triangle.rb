27674ff40bff7037bb71d9a065c9619a71052a55
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-10 02:42:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,19 +16,19 @@ class Triangle
         raise TriangleError
       else 
     end 
-    if @side_1 == @side_2 && @side_2 == @side_3
-      :equilateral
-    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
-      :isosceles
-    else
-      :scalene
-    end
-    # if @side_1 == @side2 && @side_2 == @side_3
-    #     :equilateral 
-    #   elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
-    #     :isosceles 
-    #   else 
-    #     :scalene
-    # end 
+    # if @side_1 == @side_2 && @side_2 == @side_3
+    #   :equilateral
+    # elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+    #   :isosceles
+    # else
+    #   :scalene
+    # end
+    if @side_1 == @side2 && @side_2 == @side_3
+        :equilateral 
+      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
+        :isosceles 
+      else 
+        :scalene
+    end 
   end 
 end 
