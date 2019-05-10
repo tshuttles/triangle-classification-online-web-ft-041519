0df493e4491c8cf47efca29800a4e33c36b3cffe
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-10 02:38:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,8 +20,7 @@ class Triangle
         :equilateral 
       elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
         :isosceles 
-      else @side_1 != @side_2 && @side_2 != @side_3 && @side1 != @side_3 
-        :scalene 
+      else 
     end 
   end 
 end 
